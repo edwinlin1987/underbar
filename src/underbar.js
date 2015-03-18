3978c9dbd91da0c46e464c23d06e32f165752317
Edwin <edwinlin1987@gmail.com>
2015-03-18 16:12:50 -0700

    Underbar.js - Fixed identity, last, each, and filter functions

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.slice(n > array.length ? 0 : (array.length - n), array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,20 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection) == true) {
+      var i;
+      for (i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
+    else if (typeof(collection) == 'object') {
+      var i;
+      for (i in collection) {
+        iterator(collection[i], i, collection);
+      }
+    }
+
+    
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,6 +82,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var list = [];
+
+    _.each(collection, function(item) {
+      if (test(item) == true) {
+        list.push(item);
+      }
+    });
+    return list;
   };
 
   // Return all elements of an array that don't pass a truth test.
